83bd7eb6d5ce4966b6623be0341f110da9112092
Ken Guie <nyhunter77@yahoo.com>
2016-06-11 14:37:03 -0400

    changed commentbox to container, time to add reducer

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class CommentBox extends Component {
   constructor(props) {
@@ -14,6 +16,7 @@ class CommentBox extends Component {
   handleSubmit(event) {
     event.preventDefault();
 
+    this.props.saveComment(this.state.comment); // action creator
     this.setState({ comment: '' });
   }
 
@@ -31,4 +34,6 @@ class CommentBox extends Component {
   }
 }
 
-export default CommentBox;
+export default connect(null, actions)(CommentBox);
+// null because we're not bringing in any state
+// forgo the mapDispatchToProps function by using * as actions above - you have just brought in all actions
